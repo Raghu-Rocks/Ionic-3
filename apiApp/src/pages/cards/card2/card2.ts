375208bb212d72c769548aa9e3a0b34c6183c0bf
ADOBENET\ranandan <ranandan@adobe.com>
2017-07-01 21:46:42 +0530

    getting data dynamically

--- apiApp/src/pages/cards/card2/card2.ts
+++ apiApp/src/pages/cards/card2/card2.ts
@@ -21,7 +21,7 @@ import * as c3 from 'c3';
 })
 export class Card2Page {
   
-  @Input() hero2: PeopleServiceProvider;
+  @Input() hero2:any = PeopleServiceProvider;
   @Input('index') card_index:any;
   @Input('master') masterName: string;
   @ViewChild ('#dashboardCharts')dashboardChart: ElementRef;
@@ -33,7 +33,7 @@ export class Card2Page {
     public navParams: NavParams, 
     public peopleServiceProvider: PeopleServiceProvider) {
     
-      
+  
   }
 ngAfterViewInit() {
 //   console.log(this.card_index,'card_index1');
@@ -72,12 +72,48 @@ ngAfterViewInit() {
 // }
   navigateToDetailPage(){
     this.navCtrl.push("DetailScreen2Page", this.hero2)
-    console.log(this.hero2, "hero")
-  }
+  //  console.log(this.hero2, "hero")
+}
+
+public keyValue:any;
+
   mapjson (){
-    let json = this.hero2;
-    let objs = Object.keys(json).map(key => json[key]); 
-    console.log(objs[4]['name'], "object");
+   // let json = this.hero2;
+ //  console.log("inside mapjon start");
+  //  console.log(JSON.parse(JSON.stringify( this.hero2)));
+  //    console.log("inside mapjon end");
+  //    let array = this.hero2;
+//debugger;
+
+  for (var index = 0; index < this.hero2.collection.length; index++) {
+      // console.log("INDEX ="+index);
+      let data:any =  this.hero2.collection[index];
+    //  console.log( this.hero2.collection[index]);
+      //  let suraj = Object.entries(data );
+      //  console.log("lkdsjfsd =" + suraj);
+  }
+  
+
+  let raghu = Object.entries(this.hero2.collection[0])
+    // console.log("raghu =" + Object.entries(this.hero2.collection[0]));
+    let suraj = Object.entries(raghu[2][1]);
+    // console.log("suraj =" +  suraj[0][1]);
+    //  let suarabh =suraj[0][1];
+      // console.log("suarabh =" +  suarabh[0][0]);
+
+    let suarabh =  Object.entries(suraj[0][1]); 
+    // console.log("suarabh =" +  suarabh[0][1]);
+
+  let gst = Object.entries( suarabh[0][1]);
+
+  console.log("gst =" +  gst[2]);
+    
+    
+    
+    
+       //  console.log("raghu =" + Object.entries(this.hero2.collection[1] ));
+  //  let objs = Object.keys(json).map(key => json[key]); 
+ //   console.log(objs, "object json");
     // var filters = [{"user":"abc"},{"application":"xyz"}];
     // for (let key in filters){
     //     console.log( key + ": " + filters[key], "key");
